4f15cfa8e7c676158c475ae87232003340bec123
Habibur Rahman Bahar <55600689+habiburbahar@users.noreply.github.com>
2022-01-09 14:19:57 -0600

    MainUserInterface and its function have been added

---
+++ src/main/java/com/example/drawing_programjavafx/MainUserInterface.java
@@ -0,0 +1,56 @@
+package com.example.drawing_programjavafx;
+
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+
+public class MainUserInterface extends StackPane {
+    ToolView tools;
+    ColorView colours;
+    DrawingView drawing;
+
+    public MainUserInterface() {
+        tools = new ToolView();
+        colours = new ColorView();
+        drawing = new DrawingView();
+        tools.setMinWidth(60);
+        //tools.setPrefWidth(160);
+        colours.setMinWidth(60);
+        BorderPane root = new BorderPane();
+        root.setLeft(tools);
+        root.setCenter(drawing);
+        root.setRight(colours);
+        this.getChildren().add(root);
+    }
+
+    public void setModel(ShapeModel newModel) {
+        tools.setModel(newModel);
+        drawing.setModel(newModel);
+    }
+
+    public void setInteractionModel(InteractionModel newIModel) {
+        tools.setInteractionModel(newIModel);
+        colours.setInteractionModel(newIModel);
+        drawing.setInteractionModel(newIModel);
+    }
+
+    public void setInitialTool() {
+        tools.setInitialTool();
+    }
+
+    public void setControllers(DrawingController mainViewController, MiniDrawingController miniController) {
+        tools.setController(mainViewController);
+        colours.setController(mainViewController);
+        drawing.setControllers(mainViewController,miniController);
+    }
+
+    @Override
+    public void layoutChildren() {
+        super.layoutChildren();
+        System.out.println("MainUI: Width: " + this.getWidth() + "  Height: " + this.getHeight());
+        drawing.doResize(this.getWidth()-120, this.getHeight());
+    }
+
+    public void setInitialColour() {
+        colours.setInitialColour();
+    }
+}
